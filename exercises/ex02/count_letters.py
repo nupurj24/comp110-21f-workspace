bcb1b97f3d65d3b778f8dd0995df754efb0491f5
Nupur Joshi <nupurj24@email.unc.edu>
2021-09-07 11:30:35 -0400

    Progress on Exercise 2

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -4,7 +4,7 @@ __author__ = "730391424"
 
 
 # Begin your solution here...
-letter: str = input("What letter do you want to search for? ")
+letter: str = input("What letter do you want to search for?: ")
 word: str = input("Enter a word: ")
 i = int = 0 
 b = int = 0
@@ -12,4 +12,6 @@ while i < len(word):
     if letter == word[i]:
         b += 1
     i = i + 1
-print(b)
+# need to turn b into a string i think and remember to add b into the string
+c = str(b)
+print("Count: " + c)
