edb912b23d040097553b142289e3adce66e46e1e
Nupur Joshi <nupurj24@email.unc.edu>
2021-09-22 12:54:49 -0400

    Progress on PJ00

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -14,33 +14,27 @@ def greet() -> None:
     """Greet and welcome the player."""
     print("Hello! Welcome to our fun fair! In this game you will decide which parts of the fair to explore and earn points as you make your choices! \U0001F3A1\U0001F3A2")
     global player
-    player = str = input("Before we begin (and for 5 starter points), what is your name? ")
-    print(f"Welcome to the game, {player}") 
-    global points
-    points = points + 5
+    player = input("Before we begin, what is your name? ")
+    print(f"Welcome to the game, {player}!") 
 
 
-def main() -> None:
-    greet()
-    points: int = 0 
-    path: str = input(f"{player}, would you like to... one: Ride the Ferris Wheel. two: Go to the Tarot Card Booth. three: Get funnel cake. or four: Quit game. Type the option you are selecting in lowercase with no spaces or colons please! ")
-    if path == "one":
-        ferris_wheel()
-    if path == "two":
-        tarot()
-    if path == "three":
-        funnel_cake()
-    if path == "four":
-        print(f"Goodbye {player}! Thank you so much for playing! You earned {points} points! Amazing job!")
+def ferris_wheel() -> int:
+    """Ferris Wheel Choices"""
+    global points 
+    print(f"Welcome to the Ferris Wheel ride, {player}! Unfortunately, there is a line for this ride for 30 minutes. ")
+    choice: str = input(f"{player}, Would you like to... one: Wait in line for the ride. or two: choose another option? Please enter your choice in all lowercase with no spaces or colons.")
+    if choice == "one":
+        print(f"The wait was worth it! We hope you enjoyed the view, {player}!")
+        points = points + 20
+    else: 
+        if choice == "two":
+            print(f"That makes sense, {player}! Time is of the essence!")
+            points = points + 5
+    return points
 
 
-def ferris_wheel():
-
-
-def funnel_cake():
-
-
-def tarot(): 
+def tarot() -> int:
+    """Tarot Card Booth"""
     print("You picked the ...")
     SECRET: int = int(randint(1, 4))
     if SECRET == 1: 
@@ -55,6 +49,40 @@ def tarot():
                 if SECRET == 4: 
                     print("The Sun Tarot Card! This card relates to joy, happiness, and triumph!")
     print(f"Hmmm,{player} I wonder what that could mean for you. \U0001F914")
+    global points
+    points = points + 10
+    return points
+
+
+def funnel_cake(points: int) -> int:
+    funnel_points: int = 0
+    bogo: str = input(f"Good News! You received a buy one get one funnel cake coupon upon ticket purchase, {player}! Would you like to... one: Keep the free funnel cake for yoursef. or two: Give it to the person in line behind you? ")
+    if bogo == "one":
+        funnel_points = funnel_points + 5
+    else: 
+        if bogo == "two":
+            funnel_points = funnel_points + 30
+    return funnel_points
+
+
+def main() -> None:
+    greet()
+    global points
+    points = 0 
+    path: str = ""
+    while path != "four":
+        path = input(f"{player}, would you like to... one: Ride the Ferris Wheel. two: Go to the Tarot Card Booth. three: Get funnel cake. or four: Quit game. Type the option you are selecting in lowercase with no spaces or colons please! ")
+        if path == "one":
+            ferris_wheel()
+        else:
+            if path == "two":
+                tarot()
+            else:
+                if path == "three":
+                    points = funnel_cake(int(points))
+                else:
+                    if path == "four":
+                        print(f"Goodbye {player}! Thank you so much for playing! You earned {points} points! Amazing job!")
 
 
 # def main() -> None: 
