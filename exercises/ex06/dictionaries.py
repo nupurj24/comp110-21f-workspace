77b65be130312b607f373875b1c384a568c98780
Nupur Joshi <nupurj24@email.unc.edu>
2021-10-20 15:52:31 -0400

    Progress on EX06

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -1,5 +1,56 @@
 """Practice with dictionaries."""
 
-__author__ = "123456789"
+__author__ = "730391424"
 
 # Define your functions below
+
+
+def main() -> None:
+    """Main."""
+    original_dict: dict[str, str]
+    original_dict = {"a": "b", "c": "d"}
+    print(invert(original_dict))
+    colors: dict[str, str]
+    colors = {"Nupur": "blue", "Lydia": "orange", "Ellie": "blue", "Saurya": "blue", "Meredith": "green"}
+    print(favorite_color(colors))
+    list0: list[str] = ["a", "b", "c", "c", "d"]
+    print(count(list0))
+
+
+def invert(original_dict: dict[str, str]) -> dict[str, str]:
+    inverted_dict: dict[str, str] = {}
+    for key in original_dict:
+        inverted_dict[original_dict[key]] = key
+        if original_dict[key] in original_dict:
+            raise KeyError("You can't have duplicate keys!")
+    return inverted_dict
+
+
+def favorite_color(colors: dict[str, str]) -> str:
+    favorite_color: str = ""
+    colors_dict: dict[str, int] = {}
+    for key in colors:
+        colors_dict[colors[key]] = 1
+    for key in colors: 
+        if colors[key] in colors_dict:
+            colors_dict[colors[key]] += 1
+    max: int = 1 
+    for key in colors_dict:
+        if colors_dict[key] > max:
+            max = colors_dict[key]
+            favorite_color = key
+    return favorite_color
+
+
+def count(list0: list[str]) -> dict[str, int]:
+    count_dict: dict[str, int] = {}
+    for item in list0:
+        count_dict[item] = 0
+    for item in list0:
+        if item in count_dict: 
+            count_dict[item] = count_dict[item] + 1
+    return count_dict
+
+
+if __name__ == "__main__":
+    main()
--- exercises/ex06/dictionaries_test.py
+++ exercises/ex06/dictionaries_test.py
@@ -1,6 +1,66 @@
 """Unit tests for dictionary functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex06.dictionaries import invert, favorite_color, count
+from exercises.ex06.dictionaries import invert, favorite_color, count
 
-__author__ = "123456789"
+__author__ = "730391424"
+
+
+def test_invert_basic_dictionary() -> None:
+    """Example usage."""
+    original_dict: dict[str, str] = {"a": "b", "c": "d"}
+    assert invert(original_dict) == {"b": "a", "d": "c"}
+
+
+def test_invert_dictionary_with_longer_strings() -> None:
+    """Although strings have multiple words, the strings should not invert."""
+    original_dict: dict[str, str] = {"Apple Bees": "Panera Bread", "California Pizza Kitchen": "Mediterranean Deli"}
+    assert invert(original_dict) == {"Panera Bread": "Apple Bees", "Mediterranean Deli": "California Pizza Kitchen"}
+
+
+def test_favorite_color_all_occur_same_amount() -> None: 
+    """If each color occurs the same time then the first color should return."""
+    colors: dict[str, str] = {"Nupur": "blue", "Lillian": "green"}
+    assert favorite_color(colors) == "blue"
+
+
+def test_favorite_color_all_have_different_frequencies() -> None: 
+    """If each color occurs different times, the most popular should still return."""
+    colors: dict[str, str] = {"Nupur": "blue", "Lillian": "green", "Erika": "green", "Louisa": "orange", "Jonathan": "orange", "MK": "orange"}
+    assert favorite_color(colors) == "orange" 
+
+
+def test_favorite_color_empty() -> None: 
+    """If an empty dictionary is given, an empty string should result."""
+    colors: dict[str, str] = {}
+    assert favorite_color(colors) == "" 
+
+
+def test_favorite_color_some_ties() -> None: 
+    """If there are multiple ties but also others in the dictionary, the first that occurs the most should return."""
+    colors: dict[str, str] = {"Nupur": "blue", "Lillian": "blue", "Erika": "blue", "Louisa": "orange", "Jonathan": "orange", "MK": "orange", "Meredith": "green", "Lydia": "green"}
+    assert favorite_color(colors) == "blue" 
+
+
+def test_invert_empty_dictionary() -> None:
+    """Empty dictionary should return an empty dictionary."""
+    original_dict: dict[str, str] = {}
+    assert invert(original_dict) == {}
+
+
+def test_count_empty_list() -> None:
+    """Empty list should return an empty dictionary."""
+    list0: list[str] = []
+    assert count(list0) == {}
+
+
+def test_count_equal_occurrences() -> None:
+    """If strings occur the same amount of times the count should be the same."""   
+    list0: list[str] = ["a", "a", "b", "b"]
+    assert count(list0) == {"a": 2, "b": 2}
+
+
+def test_count_basic_use() -> None:
+    """Basic use."""
+    list0: list[str] = ["a", "b", "c", "c", "d"]
+    assert count(list0) == {'a': 1, 'b': 1, 'c': 2, 'd': 1} 
